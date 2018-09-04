6ec6f09040c28d8073b5c09b463d39f4ace0045d
Altaf B <altafb2010@gmail.com>
2018-09-04 02:15:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,27 +1,6 @@
 var kittens = ['Milo', 'Otis', 'Garfield']
 
-//appends to kittens 
 
-function destructivelyAppendKitten(array) {kittens.push('Ralph') 
-return array 
-  //prepend to kittens
-}
-function destructivelyPrependKitten(array) {kittens.unshift('Bob') 
-return array
-
-//removes last kitten 
-}
-function destructivelyRemoveLastKitten(array) {kittens.pop()
-return array
-}
-
-//removes first kitten 
-function destructivelyRemoveFirstKitten(array) {kittens.shift()
-return array 
-}
 function appendKitten(array) {kittens = kittens.splice(3, 0, 'Broom') 
  return kittens  
-}
-function prependKitten(name) {kittens = kittens.splice(0)
-return kittens
 }
