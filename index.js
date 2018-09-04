f99a19038a19f54bc3dd8c8d12b708afb38b2bcd
Altaf B <altafb2010@gmail.com>
2018-09-04 02:19:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var kittens = ['Milo', 'Otis', 'Garfield']
 
 
-function appendKitten(name) {kittens = kittens.splice(3, 0, 'Broom') 
- return kittens  
+function appendKitten(name) {Broom = kittens.splice(3, 0, 'Broom') 
+ return Broom 
 }
